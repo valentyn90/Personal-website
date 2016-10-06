2a8e1a6bebb8be2a862545b9df36c36952c3687c
itsumrat <itsumrat@gmail.com>
2016-10-07 00:35:05 +0600

    rough site deploy

---
+++ js/main.js
@@ -0,0 +1,174 @@
+     $('.open-overlay').click(function() {
+       $('.open-overlay').css('pointer-events', 'none');
+       var overlay_navigation = $('.overlay-navigation'),
+         top_bar = $('.bar-top'),
+         middle_bar = $('.bar-middle'),
+         bottom_bar = $('.bar-bottom');
+
+       overlay_navigation.toggleClass('overlay-active');
+       if (overlay_navigation.hasClass('overlay-active')) {
+
+         top_bar.removeClass('animate-out-top-bar').addClass('animate-top-bar');
+         middle_bar.removeClass('animate-out-middle-bar').addClass('animate-middle-bar');
+         bottom_bar.removeClass('animate-out-bottom-bar').addClass('animate-bottom-bar');
+         overlay_navigation.removeClass('overlay-slide-up').addClass('overlay-slide-down')
+         overlay_navigation.velocity('transition.slideLeftIn', {
+           duration: 300,
+           delay: 0,
+           begin: function() {
+             $('nav ul li').velocity('transition.perspectiveLeftIn', {
+               stagger: 150,
+               delay: 0,
+               complete: function() {
+                 $('nav ul li a').velocity({
+                   opacity: [1, 0],
+                 }, {
+                   delay: 10,
+                   duration: 140
+                 });
+                 $('.open-overlay').css('pointer-events', 'auto');
+               }
+             })
+           }
+         })
+
+       } else {
+         $('.open-overlay').css('pointer-events', 'none');
+         top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
+         middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
+         bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
+         overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up')
+         $('nav ul li').velocity('transition.perspectiveRightOut', {
+           stagger: 150,
+           delay: 0,
+           complete: function() {
+             overlay_navigation.velocity('transition.fadeOut', {
+               delay: 0,
+               duration: 300,
+               complete: function() {
+                 $('nav ul li a').velocity({
+                   opacity: [0, 1],
+                 }, {
+                   delay: 0,
+                   duration: 50
+                 });
+                 $('.open-overlay').css('pointer-events', 'auto');
+               }
+             });
+           }
+         })
+       }
+     })
+
+
+// Home Page 
+var vid = document.getElementById("bgvid");
+var pauseButton = document.querySelector("#about-simran button");
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
+var image = document.getElementById('img-tag');
+
+var options = {};
+var data = [
+  Taggd.Tag.createFromObject({
+    position: { x: 0.19, y: 0.4 },
+    text: 'This is a tree',
+  }),
+  Taggd.Tag.createFromObject({
+    position: { x: 0.5, y: 0.3 },
+    text: 'Pretty sure this is also a tree',
+  }),
+  Taggd.Tag.createFromObject({
+    position: { x: 0.775, y: 0.5 },
+    text: 'Can you guess this one?',
+  }),
+];
+
+var taggd = new Taggd(image, options, data);
+
+
+// About Page
+$(function(){
+
+  window.sr = ScrollReveal();
+
+  if ($(window).width() < 768) {
+
+    if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+      $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+    }
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  } else {
+    
+    sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+  }
+  
+  sr.reveal('.js--fadeInLeft', {
+      origin: 'left',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+    sr.reveal('.js--fadeInRight', {
+      origin: 'right',
+      distance: '300px',
+      easing: 'ease-in-out',
+      duration: 800,
+    });
+
+
+});
+
+
+// Work Page
+$(document).ready(function() {
+  $('#lightgallery').lightGallery({
+    pager: true
+  });
+});
+
+
